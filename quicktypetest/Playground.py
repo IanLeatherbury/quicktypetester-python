848747c2efb7933a0e3921e86e2d5aaf2a8b02f2
Ian Leatherbury <ian.leatherbury@gmail.com>
2018-01-28 16:37:37 -0800

    refactor pokemon method

--- quicktypetest/Playground.py
+++ quicktypetest/Playground.py
@@ -1,34 +1,27 @@
-def __init__(self, prev_evolution,
-             name,
-             img,
-             spawn_chance,
-             egg,
-             weaknesses,
-             weight,
-             height,
-             candy_count,
-             avg_spawns,
-             num,
-             candy,
-             spawn_time,
-             next_evolution,
-             multipliers,
-             type,
-             id):
-    self.prev_evolution = prev_evolution,
-    self.name = name,
-    self.img = img,
-    self.spawn_chance = spawn_chance,
-    self.egg = egg,
-    self.weaknesses = weaknesses,
-    self.weight = weight,
-    self.height = height,
-    self.candy_count = candy_count,
-    self.avg_spawns = avg_spawns,
-    self.num = num,
-    self.candy = candy,
-    self.spawn_time = spawn_time,
-    self.next_evolution = next_evolution,
-    self.multipliers = multipliers,
-    self.type = type,
-    self.id = id
+
+
+    student1 = Student("Edward", "Gates", "0456789", {'math': 100, 'bio': 90, 'history': 80})
+    print student1.math  # prints 100
+    print student1.bio  # prints 90
+    
+
+    class Student(object):
+        def __init__(self, first_name, last_name, id, **kwargs):
+            self.first_name = first_name
+            self.last_name = last_name
+            self.id = id
+            for key, value in kwargs.iteritems():
+                setattr(self, key, value)
+
+    student1 = Student("Edward", "Gates", "0456789", {'math': 100, 'bio': 90, 'history': 80})
+
+
+
+
+
+def someFunc(myList = []):
+    for x in myList:
+        print x
+
+
+# someFunc(pokedex_from_json(p).pokedex)
--- quicktypetest/QuickType.py
+++ quicktypetest/QuickType.py
@@ -14,49 +14,10 @@ def pokedex_from_json(p):
     return pokemon
 
 
-def someFunc(myList = []):
-    for x in myList:
-        print x
-
-
-# someFunc(pokedex_from_json(p).pokedex)
-
-
 class Pokemon(object):
-    def __init__(self, prev_evolution,
-                 name,
-                 img,
-                 spawn_chance,
-                 egg,
-                 weaknesses,
-                 weight,
-                 height,
-                 candy_count,
-                 avg_spawns,
-                 num,
-                 candy,
-                 spawn_time,
-                 next_evolution,
-                 multipliers,
-                 type,
-                 id):
-        self.prev_evolution = prev_evolution,
-        self.name = name,
-        self.img = img,
-        self.spawn_chance = spawn_chance,
-        self.egg = egg,
-        self.weaknesses = weaknesses,
-        self.weight = weight,
-        self.height = height,
-        self.candy_count = candy_count,
-        self.avg_spawns = avg_spawns,
-        self.num = num,
-        self.candy = candy,
-        self.spawn_time = spawn_time,
-        self.next_evolution = next_evolution,
-        self.multipliers = multipliers,
-        self.type = type,
-        self.id = id
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
 
 
 def pokemon_from_json(p):
@@ -64,7 +25,5 @@ def pokemon_from_json(p):
     return pokemon
 
 
-# t = pokemon_from_json(pokedex_from_json(p['pokemon'][0]))
-
-print(p['pokemon'][0])
+print(Pokemon(**p['pokemon'][0]).candy)
 
